953729bf2ddf8f71f5967e434bb9133d7c885c88
FAMCHON Baptiste <bfamchon@gmail.com>
2024-02-14 09:24:38 +0100

    prevent redux thunk from executing many times if not necessary

--- src/lib/watch-box/usecases/get-auth-user-watch-box.usecase.ts
+++ src/lib/watch-box/usecases/get-auth-user-watch-box.usecase.ts
@@ -1,6 +1,7 @@
 import { selectAuthenticatedUser } from '@/lib/auth/reducer';
 import { createAppAsyncThunk } from '@/lib/create-app-thunk';
 import { createAction } from '@reduxjs/toolkit';
+import { selectIsUserWatchBoxLoading } from '../slices/watch-boxes.slice';
 
 export const getAuthUserWatchBoxPending = createAction<{ authUser: string }>(
   'watchBoxes/getAuthUserWatchBoxPending'
@@ -13,5 +14,12 @@ export const getAuthUserWatchBox = createAppAsyncThunk(
     dispatch(getAuthUserWatchBoxPending({ authUser }));
     const { watchBox } = await watchBoxesGateway.getUserWatchBox({ userId: authUser });
     return watchBox;
+  },
+  {
+    condition(_, { getState }) {
+      const authUser = selectAuthenticatedUser(getState());
+      const isWatchBoxLoading = selectIsUserWatchBoxLoading(authUser, getState());
+      return !isWatchBoxLoading;
+    }
   }
 );
